3438251bcc9e7ef870c9494aecc4c314ab43e5ed
Christoper Hans <christoper.hans@bukalapak.com>
2018-02-19 12:39:08 +0700

    changed string substitution to fmt.Sprintf

--- dbutil/dbutil.go
+++ dbutil/dbutil.go
@@ -18,16 +18,17 @@ func New(filename string) *Dbutil {
 	}
 }
 
-func (d *Dbutil) Prepare() {
-	db, err := sql.Open("sqlite3", "./"+d.Filename)
+func (d *Dbutil) Prepare() *sql.DB {
+	db, err := sql.Open("sqlite3", fmt.Sprintf("./%s", d.Filename))
 	_, err = db.Query("select id, name from products")
 	if err != nil {
 		db.Exec("create table products (id int primary key, product_name varchar(20))")
 	}
+  return db
 }
 
 func (d *Dbutil) Select() *sql.Rows {
-	db, err := sql.Open("sqlite3", "./"+d.Filename)
+  db := d.Prepare()
 	rows, err := db.Query("select id, product_name from products")
 	if err != nil {
 		log.Fatal(err)
@@ -51,13 +52,10 @@ func (d *Dbutil) Select() *sql.Rows {
 }
 
 func (d *Dbutil) SelectCount() int {
-	db, err := sql.Open("sqlite3", "./"+d.Filename)
-	if err != nil {
-		log.Fatal(err)
-	}
+  db := d.Prepare()
 	var result string
 	var intResult int
-	err = db.QueryRow("select count(1) from products").Scan(&result)
+  err := db.QueryRow("select count(1) from products").Scan(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -67,11 +65,8 @@ func (d *Dbutil) SelectCount() int {
 }
 
 func (d *Dbutil) Insert(id string, product_name string) {
-	db, errOpen := sql.Open("sqlite3", "./"+d.Filename)
-	if errOpen != nil {
-		log.Fatal(errOpen)
-	}
-	query_string := "insert into products (id, product_name) values (" + id + ", '" + product_name + "')"
+  db := d.Prepare()
+	query_string := fmt.Sprintf("insert into products (id, product_name) values (%s, '%s')", id, product_name)
 	_, err := db.Exec(query_string)
 	if err != nil {
 		log.Fatal(err)
@@ -80,11 +75,8 @@ func (d *Dbutil) Insert(id string, product_name string) {
 }
 
 func (d *Dbutil) Update(id string, product_name string) {
-	db, errOpen := sql.Open("sqlite3", "./"+d.Filename)
-	if errOpen != nil {
-		log.Fatal(errOpen)
-	}
-	query_string := "update products set product_name = '" + product_name + "' where id = " + id
+  db := d.Prepare()
+	query_string := fmt.Sprintf("update products set product_name = '%s' where id = %s", product_name, id)
 	_, err := db.Exec(query_string)
 	if err != nil {
 		log.Fatal(err)
@@ -93,9 +85,9 @@ func (d *Dbutil) Update(id string, product_name string) {
 }
 
 func (d *Dbutil) SelectOne(id string) string {
-	db, err := sql.Open("sqlite3", "./"+d.Filename)
+  db := d.Prepare()
 	var result string
-	err = db.QueryRow("select product_name from products where id = " + id).Scan(&result)
+  err := db.QueryRow(fmt.Sprintf("select product_name from products where id = %s", id)).Scan(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -104,11 +96,8 @@ func (d *Dbutil) SelectOne(id string) string {
 }
 
 func (d *Dbutil) Delete(id string) {
-	db, errOpen := sql.Open("sqlite3", "./"+d.Filename)
-	if errOpen != nil {
-		log.Fatal(errOpen)
-	}
-	query_string := "delete from products where id = " + id
+  db := d.Prepare()
+	query_string := fmt.Sprintf("delete from products where id = %s", id)
 	_, err := db.Exec(query_string)
 	if err != nil {
 		log.Fatal(err)
@@ -117,10 +106,7 @@ func (d *Dbutil) Delete(id string) {
 }
 
 func (d *Dbutil) DeleteAll() {
-	db, errOpen := sql.Open("sqlite3", "./"+d.Filename)
-	if errOpen != nil {
-		log.Fatal(errOpen)
-	}
+  db := d.Prepare()
 	query_string := "delete from products"
 	_, err := db.Exec(query_string)
 	if err != nil {
